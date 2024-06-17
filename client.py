5e5af3878507a119f753f9c391e3dcbeb2a2c650
bpace1 <82056912+bpace1@users.noreply.github.com>
2024-06-17 15:35:57 -0300

    client.py "ready"

--- client.py
+++ client.py
@@ -1,54 +1,102 @@
 import requests
 import sys
+import json
 from colorama import init, Fore, Style
 
-HOST: str = "127.0.0.1"
+HOST: str = "http://localhost"
 PORT: str = "8000"
 
 base_url: str = HOST + ':' + PORT
 
-def get_all_nobels(username: str, password: str):
+def get_all_nobels(username: str, password: str): #OK
     response = requests.get(f"{base_url}/all_nobels/{username}/{password}")
-    return response.json if response.status.code == 200 else (response.status, response.txt)
+    if response.status_code == 200:
+        data: dict = json.loads(response.text)
+        print(json.dumps(data, indent=4))
+    return response.status_code, response.text
 
-def get_nobel_by_year(username: str, password: str, year: str):
+def get_nobel_by_year(username: str, password: str, year: str): #OK
     response = requests.get(f"{base_url}/nobel_by_year/{username}/{password}/{year}")
-    return response.json if response.status.code == 200 else (response.status, response.txt)
+    if response.status_code == 200:
+        data: dict = json.loads(response.text)
+        print(json.dumps(data, indent=4))
+    return response.status_code, response.text
 
-def get_nobel_by_category(username: str, password: str, category: str):
+def get_nobel_by_category(username: str, password: str, category: str): #OK
     response = requests.get(f"{base_url}/nobel_by_category/{username}/{password}/{category}")
-    return response.json if response.status.code == 200 else (response.status, response.txt)
+    if response.status_code == 200:
+        data: dict = json.loads(response.text)
+        print(json.dumps(data, indent=4))
+    return response.status_code, response.text
 
-def get_all_categories(username: str, password: str):
-    response = requests.get(f"{base_url}/get_all_categories/{username}/{password}")
-    return response.json if response.status.code == 200 else (response.status, response.txt)
+def get_all_categories(username: str, password: str): #OK
+    response = requests.get(f"{base_url}/get_all_categories/{username}/{password}/")
+    if response.status_code == 200:
+        print(response.text)
+    return response.status_code, response.text
 
 def add_nobel(username: str, password: str, nobel_data: dict):
     response = requests.post(f"{base_url}/add_nobel/{username}/{password}", json = nobel_data)
-    return response.json if response.status.code == 200 else (response.status, response.txt)
+    return response.text if response.status_code == 200 else (response.status_code, response.text)
 
 def delete_nobel(username: str, password: str, nobel_data: dict):
     response = requests.delete(f"{base_url}/delete_nobel/{username}/{password}", json = nobel_data)
-    return response.json if response.status.code == 200 else (response.status, response.txt)
+    return response.text if response.status_code == 200 else (response.status_code, response.txt)
 
 def update_nobel(username: str, password: str, nobel_data: dict):
     response = requests.patch(f"{base_url}/delete_nobel/{username}/{password}", json = nobel_data)
-    return response.json if response.status.code == 200 else (response.status, response.txt)
+    return response.text if response.status_code == 200 else (response.status_code, response.txt)
 
 def validate_user(username: str, password:str):
     response = requests.get(f"{base_url}/validate_user/{username}/{password}")
-    return response.json if response.status.code == 200 else (response.status, response.txt)
+    return response.text if response.status_code == 200 else (response.status_code, response.txt)
 
 def create_user(username: str, password: str):
     response = requests.post(f"{base_url}/create_user/{username}/{password}")
-    return response.json if response.status.code == 200 else (response.status, response.txt)
+    print (response.text)
+    return response.text if response.status_code == 200 else (response.status_code, response.txt)
 
 def change_password(username: str, password: str, new_password: str):
     response = requests.post(f"{base_url}/change_password/{username}/{password}/{new_password}")
-    return response.json if response.status.code == 200 else (response.status, response.txt)
+    return response.text if response.status_code == 200 else (response.status_code, response.txt)
 
-def update_nobel(username: str, password: str, nobel_data: dict):
-    response = requests.update()
+
+def _get_nobel_data() -> list[dict]:
+    print("Ingrese datos del premio Nobel")
+    year = input("Ingrese año: ")
+    category = input("Ingrese categoría: ")
+    id: str = input("Ingrese id: ")
+    nombre: str = input("Ingrese nombre: ")
+    apellido: str = input("Ingrese apellido: ")
+    motivacion: str = input("Ingrese motivacion: ")
+    compartido: str = input("Ingrese si es compartido. 0 para no compartidos: ")
+    laureates: list[dict] = [
+        {
+          "id": id,
+          "firstname": nombre,
+          "surname": apellido,
+          "motivation": motivacion,
+          "share": compartido
+        }
+    ]
+    nobel_data = {
+        "year": year,
+        "category": category,
+        "laureates": laureates            }
+    return nobel_data
+
+def _menu_acciones() -> None:
+    print("Acciones:")
+    print(Fore.BLUE+"1. Listar Premios Nobel")
+    print(Fore.BLUE+"2. Listar categorías")
+    print(Fore.BLUE+"3. Buscar Premio Nobel por categoría")
+    print(Fore.BLUE+"4. Buscar Premio Nobel por año")
+    print(Fore.BLUE+"5. Agregar un Premio Nobel")
+    print(Fore.BLUE+"6. Modificar un Premio Nobel")
+    print(Fore.BLUE+"7. Eliminar un Premio Nobel")
+    print(Fore.RED+"8. Salir")
+
+
 
 init(autoreset=True)
 
@@ -57,15 +105,7 @@ def actions_menu(user:str, password:str):
     Menú de acciones. Solicita una entrada de teclado
     y ejecuta la acción correspondiente.
     """
-    print("Acciones:")
-    print(Fore.BLUE+"1. Listar Premios Nobel")
-    print(Fore.BLUE+"2. Listar Premios Nobel por categoría")
-    print(Fore.BLUE+"3. Buscar Premio Nobel por categoría")
-    print(Fore.BLUE+"4. Buscar Premio Nobel por año")
-    print(Fore.BLUE+"5. Agregar un Premio Nobel")
-    print(Fore.BLUE+"6. Modificar un Premio Nobel")
-    print(Fore.BLUE+"7. Eliminar un Premio Nobel")
-    print(Fore.RED+"8. Salir")
+    _menu_acciones()
 
     while True:
         try:
@@ -76,35 +116,35 @@ def actions_menu(user:str, password:str):
 
         #Lista Premios Nobel
         if teclado == 1: 
-            get_all_nobels(user, password)            
+            get_all_nobels(user, password)            #OK
 
         #Lista Premios Nobel por categoría
-        elif teclado == 2:  
-            get_nobel_by_category(user, password)
+        elif teclado == 2:                          #OK
+            get_all_categories(user, password)
 
         #Buscar Premios Nobel por categoría
         elif teclado == 3: 
             category = input("Ingrese categoría: ")
-            get_nobel_by_category(user, password, category)
+            get_nobel_by_category(user, password, category)     #OK
 
         #Buscar Premios Nobel por años
-        elif teclado == 4:
+        elif teclado == 4:                                  #OK
             year = input("Ingrese año: ")
             get_nobel_by_year(user, password, year)
             
         #Agregar Premio Nobel
-        elif teclado == 5:
-            nobel_data = input("Ingrese datos del premio Nobel: ")
+        elif teclado == 5:                          #OK
+            nobel_data = _get_nobel_data()
             add_nobel(user, password, nobel_data)
             
         #Actualizar Premio Nobel
-        elif teclado == 6:
-            nobel_data = input("Ingrese datos del premio Nobel: ")
+        elif teclado == 6:                          #PROBAR
+            nobel_data = _get_nobel_data()
             update_nobel(user, password, nobel_data)
             
-        #Borrar Premio Nobel
+        #Borrar Premio Nobel                            #PROBAR
         elif teclado == 7:
-            nobel_data = input("Ingrese datos del premio Nobel: ")
+            nobel_data = _get_nobel_data()
             delete_nobel(user, password, nobel_data)
         
         #Salir
@@ -113,7 +153,10 @@ def actions_menu(user:str, password:str):
             sys.exit()
         
         else:
+            print(Fore.RED+"####################")
             print(Fore.RED+"Por favor, ingrese una opción válida.")
+            print(Fore.RED+"####################")
+            _menu_acciones()
             continue
 
 
@@ -144,15 +187,12 @@ def main():
     else:
         username: str = input("Ingrese su nombre de usuario: ")
         password: str = input("Ingrese su contraseña: ")
-            
         if validate_user(username, password) == 200:
             print(Fore.GREEN+"Inicio de sesión exitoso")
             print(Fore.BLUE+"Bienvenido", username)
             actions_menu(username, password)
         else:
             print(Fore.RED+"Usuario o contraseña incorrectos")
-            main()
-            
-            
-
-
+            main()  
+       
+main()
